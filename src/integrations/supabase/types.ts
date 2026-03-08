b713aca496369cc8083903fcc1ec0a537ca91da3
Adil Munawar <adilfromavengers@gmail.com>
2026-03-08 10:23:19 +0000

    Enable biometric schema setup

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -23,6 +23,7 @@ export type Database = {
           id: string
           last_used_at: string | null
           public_key: string
+          updated_at: string
           user_id: string
         }
         Insert: {
@@ -33,6 +34,7 @@ export type Database = {
           id?: string
           last_used_at?: string | null
           public_key: string
+          updated_at?: string
           user_id: string
         }
         Update: {
@@ -43,6 +45,7 @@ export type Database = {
           id?: string
           last_used_at?: string | null
           public_key?: string
+          updated_at?: string
           user_id?: string
         }
         Relationships: []
